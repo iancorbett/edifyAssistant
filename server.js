374079682258dbe1ad80d595e0efe72f3bec53ec
iancorbett <iancorbett1324@gmail.com>
2025-09-09 15:59:54 -0700

    load the knowledgease form the file kb.json

--- server.js
+++ server.js
@@ -9,3 +9,5 @@ app.use(cors());
 app.use(express.json());
 
 const openai = new OpenAI({ apiKey: process.env.OPENAI_API_KEY });
+
+const KB = JSON.parse(fs.readFileSync('./kb.json', 'utf8')); //loads kb from kb.json
